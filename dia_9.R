7c2836f8234c2dd285f7f8f10fb215ba8919c58e
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2020-05-20 20:40:56 +0200

    dia_9

---
+++ dia_9.R
@@ -0,0 +1,65 @@
+# D??a 9: 20/05/2020
+# Grafico de areas apiladas
+# @EdudinGonzalo
+#---------------------------------------------
+
+
+library(tidyverse)
+library(readxl)
+library(anytime)
+library(hrbrthemes)
+library(viridis)
+library(plotly)
+
+year_2020 = read.csv("~/Downloads/volpre2020.csv", sep = ";")
+year_2019 = read.csv("~/Downloads/volpre2019.csv",  sep = ";")
+
+mercado = bind_rows(year_2019, year_2020)
+
+productos = mercado %>%
+  group_by(inicio, descripcion_genero) %>%
+  mutate(kilos = as.numeric(kilos),
+         precio_moda = as.numeric(gsub(",", ".", precio_moda))) %>%
+  summarise(kilos_media = mean(kilos),
+            precio_media = mean(precio_moda))  %>%
+  ungroup() %>%
+  mutate(descripcion_genero = str_trim(descripcion_genero, "both"),
+         inicio = anydate(inicio))  %>%
+  filter(descripcion_genero %in% c("POLLO", "HUEVOS","VACUNO CANAL", "VACUNO ELABORADO")) 
+
+mercado_plot  = mercado %>%
+  ungroup() %>%
+  count(inicio, descripcion_origen) %>%
+  mutate(inicio = anydate(inicio), 
+         descripcion_origen = str_trim(descripcion_origen, "both")) %>%
+  filter(descripcion_origen %in% c("PONTEVEDRA",
+                                   "MURCIA",
+                                   "GIPUZCOA", 
+                                   "VALENCIA",
+                                   "MADRID",
+                                   "HUELVA","
+                                   FRANCIA", 
+                                   "TOLEDO",
+                                   "CASTELLON",
+                                   "CADIZ", "ALMERIA", "PORTUGAL", "ALICANTE", 
+                                   "SEVILLA", "MALAGA")) %>%
+         ggplot(aes(x=inicio, y=n, fill=descripcion_origen)) + 
+  geom_area(alpha=0.7 , size=.1, colour="grey50") +
+  scale_fill_viridis(discrete = T, option = "plasma") +
+  labs( x = "", 
+        y = "N??mero de productos...",
+        title = "Procedencia productos Mercamadrid", 
+        subtitle = "??Cu??les son los or??genes m??s frecuentes?", 
+        caption =  "#30diasdegraficos \n Fuente: Mercamadrid | @EdudinGonzalo") +
+  theme_ipsum() +
+  theme(legend.position = "bottom", 
+        legend.title = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) 
+
+ggsave("figs/mercado_plot.png", mercado_plot, width = 9)
+
+
+
+
+
